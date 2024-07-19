7716c5e4d44c1ed052455a722dcf1002715c3d88
darkmage <165251+mikedesu@users.noreply.github.com>
2024-07-19 17:42:11 -0500

    updated template to have is_prime()

---
+++ 13/main.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+  cout << "evildojo " << endl;
+
+  return 0;
+}
--- template.cpp
+++ template.cpp
@@ -4,10 +4,26 @@
 
 using std::cout;
 using std::endl;
+using std::sqrt;
+using std::vector;
+
+bool is_prime(unsigned long int n);
 
 int main() {
-
   cout << "evildojo " << endl;
-
   return 0;
+}
+
+bool is_prime(unsigned long int n) {
+  if (n < 2) {
+    return false;
+  }
+  // for (int i = 2; i < n; i++) {
+  // cout << "sqrt(" << n << "): " << sqrt(n) << endl;
+  for (unsigned long int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
