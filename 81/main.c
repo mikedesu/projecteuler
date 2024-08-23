515983a7a901f64b4bd40e196494bb48d8905414
darkmage <165251+mikedesu@users.noreply.github.com>
2024-08-23 01:22:24 -0500

    code golfed 81

--- 81/main.c
+++ 81/main.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
-unsigned long int matrix[80][80], path_sums[80][80];
-unsigned long int traverse_lattice(int i, int j) {
-  unsigned long int v = 0, a = 0, b = 0;
-  if (i == 79 && j == 79) {
-    v = path_sums[i][j] = matrix[i][j];
-  } else if (j == 79) {
-    v = path_sums[i][j];
-    v = v == 0 ? matrix[i][j] + traverse_lattice(i + 1, j) : v;
-    path_sums[i][j] = v;
-  } else if (i == 79) {
-    v = path_sums[i][j];
-    v = v == 0 ? matrix[i][j] + traverse_lattice(i, j + 1) : v;
-    path_sums[i][j] = v;
-  } else {
-    v = path_sums[i][j];
-    if (v == 0) {
-      a = traverse_lattice(i, j + 1);
-      b = traverse_lattice(i + 1, j);
-      v = a < b ? matrix[i][j] + a : matrix[i][j] + b;
-      path_sums[i][j] = v;
+int m[80][80],p[80][80];
+int f(int i,int j){
+  int v=0,a=0,b=0;
+  if(i==79&&j==79)
+    p[i][j]=m[i][j];
+  else if(j==79)
+    p[i][j]=p[i][j]?p[i][j]:m[i][j]+f(i+1,j);
+  else if(i==79)
+    p[i][j]=p[i][j]?p[i][j]:m[i][j]+f(i,j+1);
+  else{
+    if(!p[i][j]){
+      a=f(i,j+1);
+      b=f(i+1,j);
+      p[i][j]=(a<b)*a+(a>=b)*b+m[i][j];
     }
   }
-  return v;
+  return p[i][j];
 }
-void main() {
-  FILE *file = fopen("matrix.txt", "r");
-  for (int i = 0; i < 80; i++)
-    for (int j = 0; j < 80; j++)
-      fscanf(file, "%lu", &matrix[i][j]);
-  printf("Minimum sum: %lu\n", traverse_lattice(0, 0));
+void main(){
+  FILE *a=fopen("matrix.txt","r");
+  for(int i = 0;i<80;i++)
+    for(int j=0;j<80;j++)
+      fscanf(a,"%d",&m[i][j]);
+  printf("%d",f(0, 0));
 }
